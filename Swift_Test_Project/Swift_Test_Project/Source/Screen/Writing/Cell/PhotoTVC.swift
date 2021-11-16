f6bfb16ba69f544625e0c8420011fed0e564c27f
suyeon <rlatndus9911@naver.com>
2021-11-16 19:02:35 +0900

    [#4] 코드정리

--- Swift_Test_Project/Swift_Test_Project/Source/Screen/Writing/Cell/PhotoTVC.swift
+++ Swift_Test_Project/Swift_Test_Project/Source/Screen/Writing/Cell/PhotoTVC.swift
@@ -11,8 +11,7 @@ import SnapKit
 import Then
 
 final class PhotoTVC: UITableViewCell {
-    
-    // PhotoTVC 안에 collectionview 중첩 !
+
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -22,22 +21,22 @@ final class PhotoTVC: UITableViewCell {
         collectionView.register(cell: PhotoCVC.self)
         return collectionView
     }()
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setLayouts()
         setDelegation()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
 extension PhotoTVC {
-    private  func setDelegation() {
+    private func setDelegation() {
         collectionView.delegate = self
-        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 }
 
@@ -48,7 +47,7 @@ extension PhotoTVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 1
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: PhotoCVC = collectionView.dequeueReusableCell(forIndexPath: indexPath)
         return cell
@@ -59,30 +58,31 @@ extension PhotoTVC: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 60, height: 60)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 31, left: 16, bottom: 18, right: 0)
     }
-    
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 10
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
 }
 
 extension PhotoTVC {
+
     private func setLayouts() {
         setViewHierarchies()
         setConstraints()
     }
+
     private func setViewHierarchies() {
         contentView.addSubview(collectionView)
     }
-    
+
     private func setConstraints() {
         collectionView.snp.makeConstraints {
             $0.edges.equalToSuperview()
--- Swift_Test_Project/Swift_Test_Project/Source/Screen/Writing/WritingVC.swift
+++ Swift_Test_Project/Swift_Test_Project/Source/Screen/Writing/WritingVC.swift
@@ -11,21 +11,21 @@ import SnapKit
 import Then
 
 final class WritingVC: UIViewController {
-    
-    private lazy var closeButton = UIButton().then{
+
+    private lazy var closeButton = UIButton().then {
         $0.setImage(UIImage(systemName: "xmark"), for: .normal)
         $0.contentMode = .scaleToFill
         $0.imageEdgeInsets = UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
     }
 
-    private lazy var doneButton = UIButton().then{
+    private lazy var doneButton = UIButton().then {
         $0.setTitle("완료", for: .normal)
         $0.setTitleColor(Color.daangnOrange, for: .normal)
         $0.setTitleColor(Color.daangnGray3, for: .disabled)
         $0.titleLabel?.font = .systemFont(ofSize: 15, weight: .semibold)
     }
-    
-    private lazy var tableView = UITableView().then{
+
+    private lazy var tableView = UITableView().then {
         $0.allowsSelection = false
         $0.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
         $0.registerReusableCell(PhotoTVC.self)
@@ -33,12 +33,12 @@ final class WritingVC: UIViewController {
         $0.registerReusableCell(TagTVC.self)
         $0.registerReusableCell(TextViewTVC.self)
     }
-    
+
     private let wordButton = UIButton().then {
         $0.setTitle("자주 쓰는 문구", for: .normal)
         $0.setImage(Image.frequentlyIcon, for: .normal)
     }
-    
+
     private let townButton = UIButton().then {
         $0.setTitle("보여줄 동네 설정", for: .normal)
         $0.setImage(Image.neighborSettingIcon, for: .normal)
@@ -46,35 +46,11 @@ final class WritingVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setNavigationBar()
         setDelegation()
         setLayouts()
         setToolbar()
         tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 1))
-      
-    private let tableView = UITableView().then {
-        // 셀 선택
-        $0.allowsSelection = false
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setNavigationBar()
-    }
-}
-
-extension WritingVC {
-    private func setNavigationBar() {
-        title = "중고거래 글쓰기"
-        navigationController?.navigationBar.titleTextAttributes = [.font: UIFont.systemFont(ofSize: 19, weight: .regular)]
-        navigationItem.leftBarButtonItem = makeBarButtonItem(with: closeButton)
-        navigationItem.rightBarButtonItem = makeBarButtonItem(with: doneButton)
-    }
-    
-    private func makeBarButtonItem(with button: UIButton) -> UIBarButtonItem {
-        button.frame = CGRect(x: 0, y: 0, width: 44, height: 44)
-        button.addTarget(self, action: #selector(buttonDidTapped(_:)), for: .touchUpInside)
-        return UIBarButtonItem(customView: button)
     }
 }
 
@@ -87,23 +63,38 @@ extension WritingVC {
         fixedSpace.width = 10
         setToolbarItems([makeToolBarButtonItem(with: wordButton), fixedSpace, makeToolBarButtonItem(with: townButton), flexibleSpace], animated: true)
     }
+
     private func setDelegation() {
         tableView.delegate = self
         tableView.dataSource = self
     }
+
+    private func setNavigationBar() {
+        title = "중고거래 글쓰기"
+        navigationController?.navigationBar.titleTextAttributes = [.font: UIFont.systemFont(ofSize: 19, weight: .regular)]
+        navigationItem.leftBarButtonItem = makeBarButtonItem(with: closeButton)
+        navigationItem.rightBarButtonItem = makeBarButtonItem(with: doneButton)
+    }
+
+    private func makeBarButtonItem(with button: UIButton) -> UIBarButtonItem {
+        button.frame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        button.addTarget(self, action: #selector(buttonDidTapped(_:)), for: .touchUpInside)
+        return UIBarButtonItem(customView: button)
+    }
+
     private func makeToolBarButtonItem(with button: UIButton) -> UIBarButtonItem {
         button.titleLabel?.font = .systemFont(ofSize: 12, weight: .semibold)
         button.setTitleColor(Color.daangnBlack1, for: .normal)
         button.addTarget(self, action: #selector(buttonDidTapped(_:)), for: .touchUpInside)
-        
+
         let spacing: CGFloat = 6
         button.contentEdgeInsets =  UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: spacing)
         button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -spacing, bottom: 0, right: spacing)
         button.titleEdgeInsets = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: -spacing)
         return UIBarButtonItem(customView: button)
     }
-    
 }
+
 extension WritingVC {
     @objc
     private func buttonDidTapped(_ sender: UIButton) {
@@ -111,7 +102,7 @@ extension WritingVC {
         case closeButton:
             dismiss(animated: true, completion: nil)
         case doneButton:
-            // todo: 통신 구현시 disabled 구현
+            // TODO:- 통신 구현시 disabled 구현
             dismiss(animated: true, completion: nil)
         default:
             break
@@ -134,14 +125,13 @@ extension WritingVC: UITableViewDelegate {
             return 0
         }
     }
-    
 }
 
 extension WritingVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 7
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.row {
         case 0:
@@ -160,18 +150,19 @@ extension WritingVC: UITableViewDataSource {
                 $0.centerY.equalToSuperview()
                 $0.leading.equalToSuperview().inset(16)
             }
-            
+
             let arrowImageView = UIImageView().then {
                 $0.image = UIImage(systemName: "chevron.right")
                 $0.contentMode = .scaleAspectFit
                 $0.tintColor = .black
             }
-            cell.contentView.addSubviews(arrowImageView)
+            cell.contentView.addSubview(arrowImageView)
             arrowImageView.snp.makeConstraints {
                 $0.height.equalTo(16)
                 $0.centerY.equalToSuperview()
                 $0.trailing.equalToSuperview().inset(16)
             }
+
             return cell
         case 3:
             let cell: TextFieldTVC = tableView.dequeueReusableCell(indexPath: indexPath)
@@ -183,7 +174,7 @@ extension WritingVC: UITableViewDataSource {
             cell.makeTagButtons(with: "중고", "미개봉", "상태 양호", "하자 약간")
             cell.type = .title
             return cell
-            
+
         case 5:
             let cell: TagTVC = tableView.dequeueReusableCell(indexPath: indexPath)
             cell.makeTagButtons(with: "대면", "택배")
@@ -197,11 +188,11 @@ extension WritingVC: UITableViewDataSource {
             return UITableViewCell()
         }
     }
+
 }
 
-// 사이즈 동적 조절 개 멋져..
 extension WritingVC: TableViewCellDelegate {
-    func updateTextViewHeight(_ cell: TextViewTVC , _ textView: UITextView) {
+    func updateTextViewHeight(_ cell: TextViewTVC, _ textView: UITextView) {
         let size = textView.bounds.size
             let newSize = tableView.sizeThatFits(CGSize(width: size.width,
                                                         height: CGFloat.greatestFiniteMagnitude))
@@ -219,14 +210,15 @@ extension WritingVC {
         setViewHierarchies()
         setConstraints()
     }
-    
+
     func setViewHierarchies() {
         view.addSubviews(tableView)
     }
-    
+
     func setConstraints() {
-        tableView.snp.makeConstraints{
+        tableView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+
     }
 }
